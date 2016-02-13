72528785c3a7e733f90f825f5bad2be19d7fb47d
Richard Alam <ritzalam@gmail.com>
2016-02-13 04:18:58 -0500

    - calculate checksum

--- util.go
+++ util.go
@@ -3,6 +3,7 @@ package main
 import "strings"
 import "crypto/sha1"
 import "fmt"
+import "sort"
 
 func stripChecksum(queryString, checksum string) string {
 	if (queryString != "" ) {
@@ -20,4 +21,23 @@ func calculateChecksum(str string) string {
 	fmt.Printf("%x\n", checksum)
 	s := fmt.Sprintf("%x", checksum)
 	return s
+}
+
+func sortMap(data map[string]string) map[string]string {
+	// To store the keys in slice in sorted order
+    var keys []string
+    for k := range data {
+        keys = append(keys, k)
+    }
+    sort.Strings(keys)
+
+	sorted := make(map[string]string)
+
+    // To perform the opertion you want
+    for _, k := range keys {
+        fmt.Println("Key:", k, "Value:", data[k])
+        sorted[k] = data[k]
+    }
+
+    return sorted
 }
